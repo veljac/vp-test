ad1d9454f24615fc8632296eed27bab3f36050fe
Veljko Petrović <veljac@gmail.com>
2014-04-23 22:48:16 +0200

    sample points management

---
+++ dodaj/NfieldFieldworkOfficesManagement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using Nfield.Extensions;
+using Nfield.Models;
+using Nfield.Services;
+
+namespace dodaj
+{
+    public class NfieldFieldworkOfficesManagement
+    {
+        private readonly INfieldFieldworkOfficesService _foService;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public NfieldFieldworkOfficesManagement(INfieldFieldworkOfficesService foService)
+        {
+            _foService = foService;
+        }
+
+        /// <summary>
+        /// Performs query operation for available <see cref="FieldworkOffices"/>s asynchronously. 
+        /// Note that this sample does not return the result, although your real class will do so.
+        /// </summary>
+        public IEnumerable<FieldworkOffice> QueryForOfficesAsync()
+        {
+            // execute async call
+            Task<IQueryable<FieldworkOffice>> task = _foService.QueryAsync();
+
+            // get async call result
+            IEnumerable<FieldworkOffice> allOffices = task.Result.ToList();
+            return allOffices;
+        }
+    }
+}
